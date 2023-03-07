b1640482ac03ab5f7fbcd107236f2ace54db3949
biloubed <45163267+biloubed@users.noreply.github.com>
2023-03-07 15:41:37 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo("AIIE MIGUELLLL");
+echo("<br>");
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+
+</head>
+<body>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/H-L2RxUtdwc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</body>
+</html>
